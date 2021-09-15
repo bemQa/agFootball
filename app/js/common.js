bb6b55284288751cdf195a920aa4ff098ffebe8b
HubArtWork <68958381+HubArtWork@users.noreply.github.com>
2021-09-15 11:12:54 +0300

    fix bugs 3.3

--- app/js/common.js
+++ app/js/common.js
@@ -87,7 +87,12 @@ $(document).ready(function(){
 
 	//menu mob
 	$('.header-bar,.header-close').on('click', function(){
-    $('html,body').toggleClass('scroll-hidden');
+
+    if($('html').hasClass('scroll-hidden'))
+      $('html,body').removeClass('scroll-hidden');
+    else
+      $('html,body').addClass('scroll-hidden');
+
 		$('.header').toggleClass('header-active');
 	});
 
@@ -141,7 +146,9 @@ $(document).ready(function(){
 
 		var id = '#' + $(this).attr('href');
 
-    $('html, body').addClass('scroll-hidden');
+    if(!$('.header').hasClass('header-active'))
+      $('html,body').removeClass('scroll-hidden');
+
 		$(id).addClass('open');
 		$('.modal-overlay').addClass('open-overlay');
 	});
@@ -149,10 +156,12 @@ $(document).ready(function(){
 	$('.modal-close, .fancybox-close, .modal-cross').on('click',function(e){
     e.preventDefault();
 
+    if(!$('.header').hasClass('header-active'))
+      $('html,body').removeClass('scroll-hidden');
+
 		$(modalCont).removeClass('open');
 		$('.modal-overlay').removeClass('open-overlay');
 		$('.intermediate').removeClass('intermediate');
-    $('html, body').removeClass('scroll-hidden');
 	});
 
   if ($('.select').length > 0) {
